2bdf145d7bbb43f850df7966795dbea5c2c4b8a5
Aditerx <phattai3178@gmail.com>
2021-12-21 00:04:10 +0700

    Upload Dec 21, 2021

---
+++ js.js
@@ -0,0 +1,35 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+
+    $routeProvider
+
+        .when("/res",{
+            templateUrl: "residential.html",
+            controller: "addCtrl"
+        })
+        .when("/com",{
+            templateUrl: "commercial.html",
+            controller: "addCtrl"
+        })
+        .when("/mets",{
+            templateUrl: "methods.html",
+            controller: "addCtrl"
+        })
+        .when("/dev",{
+            templateUrl: "development.html",
+            controller: "addCtrl"
+        })
+        .when("/cont",{
+            templateUrl: "contact_us.html",
+            controller: "addCtrl"
+        });
+});
+
+app.controller("mainCtrl", function($scope, $http) {
+
+    $http.get("json.json").then(function(response) {
+        $scope.Data = response.data;
+        return $scope.Data;
+    });
+});
